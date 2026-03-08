e1cf1ef859972a89dac300ee7dc1b366f2459272
Both-Eyes <2823816ON@GMAIL.COM@example.com>
2026-03-08 15:43:52 -0400

    Blog: fix blog-title.js to look up by slug for author-anchored URLs

--- blog/blog-title.js
+++ blog/blog-title.js
@@ -1,13 +1,10 @@
 (function() {
   if (typeof BLOG_DATA === 'undefined') return;
   var slug = location.pathname.replace(/^\/blog\//, '').replace(/\/$/, '');
-  var key = slug.replace(/^how-(.+)-writers-write$/, 'HOW-$1').toUpperCase().replace(/-/g, '-');
-  if (slug.startsWith('how-')) {
-    key = 'HOW-' + slug.replace(/^how-/, '').replace(/-writers-write$/, '').toUpperCase();
-  } else {
-    key = slug.toUpperCase();
-  }
-  var entry = BLOG_DATA[key];
+  var entry = null;
+  Object.keys(BLOG_DATA).forEach(function(k) {
+    if (BLOG_DATA[k].slug === slug) entry = BLOG_DATA[k];
+  });
   if (!entry) return;
   document.title = entry.title + ' | Quirrely';
   var h1 = document.querySelector('h1');
